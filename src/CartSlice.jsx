212ac5209ebf6b2cfd48b2d8f9ecdbd2b9fc9153
Amit Sama <amitsama.tum@gmail.com>
2025-06-01 14:50:20 -0400

    Course 5, module 4, task 3, deploy on github

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -6,14 +6,28 @@ export const CartSlice = createSlice({
     items: [], // Initialize items as an empty array
   },
   reducers: {
+    // state stored in "items" array, 
+    // new data in "action.payload"
     addItem: (state, action) => {
-    
+        const {name, image, cost } = action.payload;
+        const existingItem = state.items.find((item) => item.name === name);
+        if (existingItem) {
+            existingItem.quantity++;
+        } else {
+            state.items.push({name, image, cost, quantity : 1});
+        }
+        //items.push({...items, plant : state});
     },
     removeItem: (state, action) => {
+        state.items = state.items.filter((item) => item.name !== action.payload.name);
     },
-    updateQuantity: (state, action) => {
 
-    
+    updateQuantity: (state, action) => {
+        const {name, cost } = action.payload;
+        const foundItem = state.items.find((item) => item.name === name);
+        if (foundItem) {
+            foundItem.quantity = action.payload.quantity;
+        }
     },
   },
 });
